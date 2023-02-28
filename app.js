df72585f1a43c2cfb85a2363d03313bf349ef4a4
Laura V. Buttner J <laurita.buttner@gmail.com>
2023-02-27 21:25:26 -0300

    Agregando espacios

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const app = express();
 app.get("/makers/:nombre", (req,res) => {
     let nombre = req.params.nombre
     nombre = nombre.charAt(0).toUpperCase() + nombre.slice(1)
-    res.send(`<h1>Hola ${nombre}! <\h1>`)
+    res.send(`<h1>Hola ${nombre} ! <\h1>`)
 })
 
 app.listen(3000,() => console.log("escuchando"))
